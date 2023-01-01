9669215f8f63121060e5cfbc25eb93c00aa2638b
Thaddeus Cooper <tcooper@nomoreboxes.com>
2023-01-01 15:01:16 -0800

    Add files via upload
    An empty starting project for the examples.

---
+++ SwiftUIExamples/SwiftUIExamples/SwiftUIExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExamplesApp.swift
+//  SwiftUIExamples
+//
+//  Created by Thaddeus Cooper on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
