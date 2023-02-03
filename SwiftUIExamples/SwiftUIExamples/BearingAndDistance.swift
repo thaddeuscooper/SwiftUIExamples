b8e2f72470b2fc2624d622d93f5017024c2fb369
Thaddeus O. Cooper <tcooper@nomoreboxes.com>
2023-02-03 14:45:53 -0800

    Started using library value of Pi (Double.pi)

--- SwiftUIExamples/SwiftUIExamples/BearingAndDistance.swift
+++ SwiftUIExamples/SwiftUIExamples/BearingAndDistance.swift
@@ -21,8 +21,8 @@ struct BearingAndDistance: View {
 		let frameWidth = 75.0
 		let frameHeight = 75.0
 		let frameDistance = frameWidth / 2.0
-		let offsetRotation = 90 * (3.1415926 / 180)
-		let multiplier = 3.1415926 / 180.0
+		let offsetRotation = 90 * (Double.pi / 180.0)
+		let multiplier = Double.pi / 180.0
 		
 		let offsetX = cos((bearing.value * multiplier) - offsetRotation) * frameDistance
 		let offsetY = sin((bearing.value * multiplier) - offsetRotation) * frameDistance
